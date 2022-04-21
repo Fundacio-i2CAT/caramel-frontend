f895d124fa797cee5909543a50a32ff91764886f
JordiMarias <jordimarias@gmail.com>
2022-04-21 12:57:12 +0200

    first compiled version

---
+++ env.js
@@ -0,0 +1,11 @@
+(function (window) {
+    window.__env = window.__env || {};
+  
+    // API url
+    window.__env.apiUrl = 'ws://192.168.115.92:8654';
+  
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window.__env.enableDebug = true;
+  }(this));
+  
