522912f1b4329a26af1adda7d67068655d92460b
Jordi Marias I Parella <jordi.marias@i2cat.net>
2022-05-04 20:19:04 +0200

    Good backend

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
     window.__env = window.__env || {};
   
     // API url
-    window.__env.apiUrl = 'ws://192.168.115.92:8654';
+    window.__env.apiUrl = 'ws://84.88.36.126:8655';
   
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
